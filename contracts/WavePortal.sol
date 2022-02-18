2c53be6dcecff91b369c87e223fad65053ef17fe
NotRithik <rithikum123@gmail.com>
2022-02-18 09:17:50 +0530

    Print 'funny' message 🖨

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.12;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Yo dawg, I'm a contract. A smart one at that!");
+    }
+}
