0b0baca50bf4b16e8e83592aad39741c2d881e36
NotRithik <rithikum123@gmail.com>
2022-02-18 09:57:13 +0530

    Write wave() and getTotalWaves() functions 👋

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.12;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
         console.log("Yo dawg, I'm a contract. A smart one at that!");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have a total of %d waves!", totalWaves);
+        return totalWaves;
     }
 }
